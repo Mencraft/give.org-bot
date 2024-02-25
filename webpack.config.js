a0b79c401f3e2d76a49e6a7e2a1c2bf28beec042
mencraft <ekemboy@gmail.com>
2024-02-25 22:54:13 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  // Entry point of your application
+  entry: './src/index.js',
+  
+  // Output configuration
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    // assetModuleFilename: 'images/[hash][ext][query]',
+  },
+  
+    // Enable source maps
+  devtool: 'source-map',
+  // Module rules for handling different file types
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Regex to match CSS files
+        use: [
+          // Replace 'style-loader' with MiniCssExtractPlugin.loader
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              // Enable source maps for CSS
+              sourceMap: true,
+            },
+          },
+          'postcss-loader'
+        ], // Use these loaders for CSS files
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]' // Output images into dist/images folder
+        },
+        include: path.resolve(__dirname, 'public'), // Include images from the public/images folder
+      },
+    ],
+  },
+    // Plugins configuration
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+    ],
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin(), // For JS
+        new CssMinimizerPlugin(), // For CSS
+      ],
+    },
+};
